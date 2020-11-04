e0cf00ae64993122b9f8f3ae263326fa18306588
CodeyPacker <codeypacker@gmail.com>
2020-11-04 15:04:28 -0700

    Added animations to exercise cards

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -61,6 +61,7 @@ const Card = styled.div`
   background-color: #fff;
   border-radius: 4px;
   z-index: 10;
+  overflow: hidden;
   box-shadow: 0px 0px 12px 3px rgba(0, 0, 0, 0.07);
 
   @media only screen and (min-width: 700px) {
@@ -69,6 +70,7 @@ const Card = styled.div`
   }
 
   .card-header {
+    position: relative;
     display: flex;
     border-bottom: 3px solid #41d3a2;
   }
@@ -83,7 +85,8 @@ const Card = styled.div`
     padding: 15px;
     margin-bottom: -1px;
     border-top-left-radius: 4px;
-    transition: background-color .2s, font-size .2s, padding .2s;
+    transition: font-size .2s, padding .2s;
+    transition-delay: .5s;
   }
 
   .card-details {
@@ -99,6 +102,7 @@ const Card = styled.div`
     font-size: 23px;
     text-transform: capitalize;
     transition: font-size .2s;
+    transition-delay: .5s;
   }
 
   .angle {
@@ -147,37 +151,44 @@ const Card = styled.div`
   }
 
   .checkbox__input input:checked
-  + .checkbox__control svg {
-    transform: scale(1);
-  }
+  + .checkbox__control svg { transform: scale(1); }
 
   .checkbox__input input:focus
   + .checkbox__control {
     box-shadow: 0 0 0 0.05em #41d3a2, 0 0 0.15em 0.1em #41d3a2;
   }
 
+  .checkboxes {
+    max-height: 500px;
+    transition: max-height .6s, margin-top .6s, margin-bottom .6s;
+  }
+
   &.isComplete {
     .checkboxes {
-      display: none;
+      max-height: 0;
+      margin-top: 0;
+      margin-bottom: 0;
     }
 
     .exercise-number {
       padding: 5px 15px;
       font-size: 16px;
+      transition: background-color .2s;
+      background-color: #bb86fc;
     }
 
+    .card-header { border-bottom: 3px solid #bb86fc; }
+
     &.dark-theme {
-      .exercise-number {
-        background-color: #41d3a2;
-      }
-
+      .exercise-number { background-color: #41d3a2; }
       .card-header { border-bottom: 3px solid #41d3a2; }
-
       .name { font-size: 16px; }
     }
 
     @media only screen and (max-width: 700px) {
       order: -1;
+      transition: order .2s;
+      transition-delay: 1s;
     }
 
     @media only screen and (min-width: 700px) {
